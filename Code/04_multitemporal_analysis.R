e7a1ccf255b7d5a533b60633d99aac7e31001862
AnnaScalisi <162353027+AnnaScalisi@users.noreply.github.com>
2025-03-19 17:19:26 +0100

    Update 04_multitemporal_analysis.R

--- Code/04_multitemporal_analysis.R
+++ Code/04_multitemporal_analysis.R
@@ -29,15 +29,30 @@ Greenland ice melt
 gr = im.import("greenland") # Importo tutte e quattro le immagini della Groenlandia dati di temperatura a 16 bit, programma Copernicus
 
 grdif = gr[[1]] - gr[[4]] # Differenza tra la prima e l'ultima immagine
-plot(grdif)
-
 grdif = gr[[1]] - gr[[4]] # 2015 - 2000
 plot(grdif)
 # All the yellow parts are those in which there is a higher value in 2015
-
 plot(grdif, col=rocket(100))
 
-# Exporting 
+# Exporting data
+
+#-------
+
+setwd("~/Downloads/") # decido qual'è la working directory. Comando per mac
+setwd("C://utente/Downloads") # comando per windows
+setwd("~/Desktop/")
+
+getwd() # per vedere quale cartella
+
+pdf("output.pdf") # Creo un file pdf nella cartella
+plot(grdif)
+dev.off()
+
+jpeg("output.jpeg") # Creo un file jpeg nella cartella
+plot(grdif)
+dev.off()
+
+
 
 
 
