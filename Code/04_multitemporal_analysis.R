8419a913fceb425c2ebebde49face9a4684669d9
AnnaScalisi <162353027+AnnaScalisi@users.noreply.github.com>
2025-03-20 14:26:04 +0100

    Update 04_multitemporal_analysis.R

--- Code/04_multitemporal_analysis.R
+++ Code/04_multitemporal_analysis.R
@@ -34,24 +34,6 @@ plot(grdif)
 # All the yellow parts are those in which there is a higher value in 2015
 plot(grdif, col=rocket(100))
 
-# Exporting data
-
-#-------
-
-setwd("~/Downloads/") # decido qual'è la working directory. Comando per mac
-setwd("C://utente/Downloads") # comando per windows
-setwd("~/Desktop/")
-
-getwd() # per vedere quale cartella
-
-pdf("output.pdf") # Creo un file pdf nella cartella
-plot(grdif)
-dev.off()
-
-jpeg("output.jpeg") # Creo un file jpeg nella cartella
-plot(grdif)
-dev.off()
-
 
 
 
