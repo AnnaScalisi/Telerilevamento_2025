6b8846f5c445f99f63c29fab63e4963daf09407d
AnnaScalisi <162353027+AnnaScalisi@users.noreply.github.com>
2025-04-01 17:30:34 +0200

    Update 07_classification.R

--- Code/07_classification.R
+++ Code/07_classification.R
@@ -1,7 +1,11 @@
 # R code for classifying images
 
+# install.packages("patchwork")
+
 library(terra)
 library(imageRy)
+library(ggplot2)
+library(patchwork)
 
 im.list()
 
@@ -28,8 +32,29 @@ perc2006 = freq(mato2006c) * 100 / ncell(mato2006c)
 
 # human = 54%, forest = 45%
 
+# Creating dataframe
 
+class = c("Forest","Human")
+y1992 = c(83,17)
+y2006 = c(45,55)
+tabout = data.frame(class, y1992, y2006)
 
+p1 = ggplot(tabout, aes(x=class, y=y1992, color=class)) +
+  geom_bar(stat="identity", fill="white") +
+  ylim=(c(0,100)) # crea la struttura del grafico. con geom.bar scelgo il tipo di grafico
+
+p2 = ggplot(tabout, aes(x=class, y=y2006, color=class)) +
+  geom_bar(stat="identity", fill="white") +
+  ylim=(c(0,100))
+
+p1 + p2
+p1 / p2
+
+p0 = im.ggplot(mato1992)
+p00 = im.ggplot(mato2006)
+# plot della prima banda con im.ggplot funzione di imageRy
+
+p0 + p00 + p1 + p2
 
 
 
