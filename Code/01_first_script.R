8f511e58be33a2c8ae889444dc0ba1a29c4f8309
AnnaScalisi <162353027+AnnaScalisi@users.noreply.github.com>
2025-04-01 12:02:33 +0200

    Update 01_first_script.R

--- Code/01_first_script.R
+++ Code/01_first_script.R
@@ -5,7 +5,7 @@
 anna <- 2 + 3 # assign an operation to an object
 chiara <- 4 + 6
 
-anna+chiara
+anna + chiara
 
 filippo <- c(0.2, 0.4, 0.6, 0.8, 0.9)   # array
 luca <- c(100, 80, 60, 50, 10)    # array 2
